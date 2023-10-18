a7750f7100641c131b82a723135c41b0d511a4e1
Andrey Potapov <aapot@ya.ru>
2023-10-18 14:19:07 +0300

    написан файл конфигурации для БД

--- src/config.py
+++ src/config.py
@@ -1,4 +1,18 @@
-import os
+from configparser import ConfigParser
 
-DB_PWD = os.getenv('DB_PASS')
+# Название БД для проекта
 DB_NAME = "parser_vacancies"
+
+
+def config_db(filename="database.ini", section="postgresql") -> dict:
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+    if parser.has_section(section):
+        params = parser.items(section)
+        db = dict(params)
+    else:
+        raise Exception(
+            'Section {0} is not found in the {1} file.'.format(section, filename))
+    return db
